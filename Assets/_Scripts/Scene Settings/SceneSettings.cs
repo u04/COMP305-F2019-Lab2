1ea46565fe4fe51f6d313e55613ad1479e924e33
cdey1 <300920267@cencol.ca>
2019-11-09 14:34:12 -0500

    #2

---
+++ Assets/_Scripts/Scene Settings/SceneSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneSettings", menuName = "Scene/Settings")]
+public class SceneSettings : ScriptableObject
+{
+    [Header("Scene Conf")]
+    public SoundClip activeSoundClip;
+    [Header("Scoreboard Labels")]
+    public bool scoreLabelEnabled;
+    public bool livesLabelEnabled;
+    public bool highScoreLabelEnabled;
+    [Header("Scene Lables")]
+    public bool startLabelActive;
+    public bool endLabelActive;
+    [Header("scene Buttons")]
+    public bool startButtonActive;
+    public bool restartButtonActive;
+
+
+}
